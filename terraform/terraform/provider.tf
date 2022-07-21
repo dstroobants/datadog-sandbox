d5a59eb22b6ea8032d4881365f764e49a29fb22f
dstroobants <denis.stroobants@datadoghq.com>
2022-07-21 15:58:01 +0100

    Added local TF configuration

---
+++ terraform/terraform/provider.tf
@@ -0,0 +1,22 @@
+# Terraform 0.13+ uses the Terraform Registry:
+terraform {
+  required_providers {
+    datadog = {
+      source = "DataDog/datadog"
+    }
+  }
+}
+
+#output "env" {
+#  value = { for tuple in regexall("(.*)=(.*)", file(".env")) : tuple[0] => tuple[1] }
+#}
+
+locals {
+  env = { for tuple in regexall("(.*)=(.*)", file(".env")) : tuple[0] => tuple[1] }
+}
+
+# Configure the Datadog provider
+provider "datadog" {
+  api_key = local.env.DD_API_KEY
+  app_key = local.env.DD_APP_KEY
+}
